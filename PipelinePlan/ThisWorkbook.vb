36f739093e49d2f36e0449e8c653f9ddc10e0a71
He Zhang <pzyszh@163.com>
2018-03-07 00:54:46 +0800

    3

--- PipelinePlan/ThisWorkbook.vb
+++ PipelinePlan/ThisWorkbook.vb
@@ -4,7 +4,7 @@ Imports Microsoft.Office.Interop.Excel
 Public Class ThisWorkbook
 
     Private Sub ThisWorkbook_Startup() Handles Me.Startup
-
+        Call ProtectSheets()
     End Sub
 
     Private Sub ThisWorkbook_Shutdown() Handles Me.Shutdown
@@ -14,8 +14,12 @@ Public Class ThisWorkbook
 
 
     Private Sub ThisWorkbook_SheetActivate(Sh As Object) Handles Me.SheetActivate
-        If Sh.name="参数" Then
-            Me.Protect()
+        Call ProtectSheets()
+
+    End Sub
+    Sub ProtectSheets()
+        If ActiveSheet.name = "参数" Or ActiveSheet.name = "运行方案" Or ActiveSheet.name = "批次" Or ActiveSheet.name = "下载方案" Then
+            Protect([structure]:=True)
         End If
     End Sub
 End Class
